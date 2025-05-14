eba0d811771867a426b84f4082cb27436fad7ebd
hoNATHAN <nathanlho@outlook.com>
2025-05-13 20:08:33 -0400

    route tree and app

--- q-bet-site/src/main.tsx
+++ q-bet-site/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppRouter } from './routes/Router.tsx'
+
+// TODO: add the theme here?
+//
+// background light: #fbf7f4
+// background dark: #121111
+// light primary: #39a77f
+// dark primary: #26c289
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>
 )
