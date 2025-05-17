ef2abb56cdefeed16390874d6f333b3cb33e821d
hoNATHAN <nathanlho@outlook.com>
2025-05-17 13:04:32 -0400

    lint and use env file

--- q-bet-backend/app.py
+++ q-bet-backend/app.py
@@ -1,18 +1,30 @@
+import os
+import uvicorn
+
 from bson import ObjectId
 from fastapi import FastAPI, HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from pydantic import BaseModel, Field
 from models import Bet, BetOut, GameIn, GameOut, Round, RoundOut
+from dotenv import load_dotenv
 
-mongodb_uri = "mongodb://localhost:27017"
+load_dotenv()
 
-#connects to mongo on startup and closes on shutdown
+mongodb_uri = os.getenv("MONGODB_URI")
+db_name = os.getenv("DB_NAME")
+host = os.getenv("HOST")
+port = int(os.getenv("PORT"))
+debug = os.getenv("DEBUG").lower() in ("true", "1", "t")
+
+
+# connects to mongo on startup and closes on shutdown
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.mongodb = AsyncIOMotorClient(mongodb_uri)["qbet_db"]
     yield
     app.mongodb.client.close()
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -20,53 +32,56 @@ app = FastAPI(lifespan=lifespan)
 """
 Bet Endpoints ---------------------------------------------------------
 """
+
+
 @app.post("/bet", response_model=BetOut)
 async def place_bet(bet: Bet):
     doc = bet.model_dump()
     result = await app.mongodb["bets"].inest_one(doc)
     return BetOut(**doc, id=str(result.inserted_id))
 
+
 @app.get("/bet/{bet_id}", response_class=BetOut)
 async def get_bet(bet_id: str):
     doc = await app.mongodb["bests"].find_one({"_id": ObjectId(bet_id)})
     return BetOut(**doc, id=bet_id) if doc else None
 
+
 @app.get("/bets", response_model=list[BetOut])
 async def get_bets():
     bets = await app.mongodb["bets"].find().to_list(100)
-    return [
-        BetOut(**{**bet, "id": str(bet["_id"])})
-        for bet in bets
-    ]
-        
+    return [BetOut(**{**bet, "id": str(bet["_id"])}) for bet in bets]
+
+
 """
 Game Endpoints ---------------------------------------------------------
 """
+
+
 @app.post("/game", response_model=GameOut)
 async def create_game(game: GameIn):
     doc = game.model_dump()
     result = await app.mongodb["games"].insert_one(doc)
     return GameOut(**doc, id=str(result.inserted_id))
 
+
 @app.get("/game/{game_id}", response_model=GameOut)
 async def get_game(game_id: str):
     doc = await app.mongodb["games"].find_one({"_id": ObjectId(game_id)})
     return GameOut(**doc, id=game_id) if doc else None
 
+
 @app.get("/games", response_model=list[GameOut])
 async def get_games():
     games = await app.mongodb["games"].find().to_list(100)
-    return [
-        GameOut(**{**game, "id": str(game["_id"])})
-        for game in games
-    ]
+    return [GameOut(**{**game, "id": str(game["_id"])}) for game in games]
+
 
 @app.post("/game/{game_id}/rounds", response_model=RoundOut)
 async def create_game_round(game_id: str, round: Round):
     doc = round.model_dump()
     result = await app.mongodb["games"].update_one(
-        {"_id": ObjectId(game_id)},
-        {"$push": {"rounds": doc}}
+        {"_id": ObjectId(game_id)}, {"$push": {"rounds": doc}}
     )
     return RoundOut(**doc, id=str(result.upserted_id))
 
@@ -76,10 +91,8 @@ async def get_game_rounds(game_id: str):
     doc = await app.mongodb["games"].find_one({"_id": ObjectId(game_id)})
     if not doc:
         return []
-    return [
-        RoundOut(**{**round, "id": str(round["_id"])})
-        for round in doc["rounds"]
-    ]
+    return [RoundOut(**{**round, "id": str(round["_id"])}) for round in doc["rounds"]]
+
 
 @app.get("/game/{game_id}/rounds/{round_number}", response_model=RoundOut)
 async def get_game_round(game_id: str, round_number: int):
@@ -89,15 +102,15 @@ async def get_game_round(game_id: str, round_number: int):
         return RoundOut(**{**round, "id": str(round["_id"])})
     return None
 
+
 @app.get("/game/{game_id}/rounds/{round_id}", response_model=RoundOut)
 async def get_game_round_by_id(game_id: str, round_id: str):
     doc = await app.mongodb["games"].find_one(
-        {"_id": ObjectId(game_id), "rounds._id": ObjectId(round_id)},
-        {"rounds.$": 1}
+        {"_id": ObjectId(game_id), "rounds._id": ObjectId(round_id)}, {"rounds.$": 1}
     )
     if not doc or not doc.get("rounds"):
         raise HTTPException(status_code=404, detail="Round not found")
-    
+
     round = doc["rounds"][0]
     return RoundOut(**{**round, "id": str(round["_id"])})
 
@@ -108,10 +121,9 @@ async def testing():
 
 
 if __name__ == "__main__":
-    import uvicorn 
     uvicorn.run(
-                "app:app", 
-                host="127.0.0.1",
-                port=8000,
-                reload=True,
+        "app:app",
+        host=host,
+        port=port,
+        reload=debug,
     )
--- q-bet-backend/models.py
+++ q-bet-backend/models.py
@@ -4,33 +4,51 @@ from pydantic import BaseModel, Field
 Bet Models ----------------------------------------------------------
 """
 
+
 class Bet(BaseModel):
     team: str = Field(..., description="Team to bet on")
     amount: float = Field(..., gt=0, description="Amount to bet")
 
+
 class BetOut(Bet):
     id: str = Field(..., description="ID of the bet")
+
 
 """
 Game Models ----------------------------------------------------------
 """
 
-class Round(BaseModel): #we can change the fields, i just put down what made sense to me 
+
+class Round(
+    BaseModel
+):  # we can change the fields, i just put down what made sense to me
     team_a_econ: float = Field(..., gt=0, description="Team A economy")
     team_b_econ: float = Field(..., gt=0, description="Team B economy")
-    team_a_score: int = Field(..., gt=0, description="Team A score") #can combine these two into a single field
-    team_b_score: int = Field(..., gt=0, description="Team B score") #
+    team_a_score: int = Field(
+        ..., gt=0, description="Team A score"
+    )  # can combine these two into a single field
+    team_b_score: int = Field(..., gt=0, description="Team B score")  #
     map_played: str = Field(..., description="Map played")
     current_round: int = Field(..., gt=0, description="Current round")
-    team_a_kills_last_round: int = Field(..., gt=0, description="Team A kills last round")
-    team_b_kills_last_round: int = Field(..., gt=0, description="Team B kills last round")
-    team_a_assists_last_round: int = Field(..., gt=0, description="Team A assists last round")
-    team_b_assists_last_round: int = Field(..., gt=0, description="Team B assists last round")
+    team_a_kills_last_round: int = Field(
+        ..., gt=0, description="Team A kills last round"
+    )
+    team_b_kills_last_round: int = Field(
+        ..., gt=0, description="Team B kills last round"
+    )
+    team_a_assists_last_round: int = Field(
+        ..., gt=0, description="Team A assists last round"
+    )
+    team_b_assists_last_round: int = Field(
+        ..., gt=0, description="Team B assists last round"
+    )
     win_type_last_round: str = Field(..., description="Win type last round")
     last_round_winner: str = Field(..., description="Last round winner")
 
+
 class RoundOut(Round):
     id: str = Field(..., description="ID of the round")
+
 
 class GameIn(BaseModel):
     team_a: str = Field(..., description="Team A name")
@@ -39,5 +57,7 @@ class GameIn(BaseModel):
     rounds: list[Round] = Field(..., description="List of rounds")
     total_rounds: int = Field(..., gt=0, description="Total rounds played")
 
+
 class GameOut(GameIn):
     id: str = Field(..., description="ID of the game")
+
