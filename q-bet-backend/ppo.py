f57deacfa47bc71acba353857dffabec651fd42a
hoNATHAN <nathanlho@outlook.com>
2025-05-25 20:02:49 -0400

    empty ppo

---
+++ q-bet-backend/ppo.py
@@ -0,0 +1,10 @@
+from network import FeedForwardNN
+
+
+class PPO:
+    def __init__(self):
+        self.actor = FeedForwardNN
+        self.value = FeedForwardNN
+
+    def learn(self):
+        return
